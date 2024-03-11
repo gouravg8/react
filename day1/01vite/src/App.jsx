814a7a5fd6f887069be931c2ba888f639181ccac
Gourav soni <gouravsoni8476@gmail.com>
2024-03-11 20:25:16 +0530

    learning hooks

--- day1/01vite/src/App.jsx
+++ day1/01vite/src/App.jsx
@@ -1,33 +1,31 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const add = () => count < 20 && setCount(count + 1);
+  const subtract = () => count > 0 && setCount(count - 1);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img
+          src="https://i.ytimg.com/vi/lI7IIOWM0Mo/maxresdefault.jpg"
+          width={"500px"}
+          className="rounded item-center mx-auto"
+          alt=""
+          srcSet=""
+        />
       </div>
-      <h1>Gourav is learning react</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className="font-bold my-8">Gourav is learning react</h1>
+      <div className="card mx-auto w-full">
+        <button className="mx-3" onClick={add}>
+          Add {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button className="mx-3" onClick={subtract}>
+          Subtract {count}
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
