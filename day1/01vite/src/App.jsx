a66beba28e48a32c20c54a3562533617d268ac7c
Gourav soni <gouravsoni8476@gmail.com>
2024-03-12 22:49:07 +0530

    added new compo BgChanger

--- day1/01vite/src/App.jsx
+++ day1/01vite/src/App.jsx
@@ -1,22 +1,23 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
+import BgChanger from "./components/BgChanger";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   // Q. what will output of this code, when we use direct 4 setCounter methods with increase counter by 1
   // change the state by 4
-  const add = () => {
-    // i've added prev for tracking the previous state of the state variable and then changing it according to it using callback function
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-    setCount((prev) => prev + 1);
-  };
-  const subtract = () => count > 0 && setCount(count - 1);
+  // const add = () => {
+  //   // i've added prev for tracking the previous state of the state variable and then changing it according to it using callback function
+  //   // setCount((prev) => prev + 1);
+  //   // setCount((prev) => prev + 1);
+  //   // setCount((prev) => prev + 1);
+  //   setCount(count + 1);
+  // };
+  // const subtract = () => count > 0 && setCount(count - 1);
 
   return (
     <>
-      <div>
+      {/* <div>
         <img
           src="https://i.ytimg.com/vi/lI7IIOWM0Mo/maxresdefault.jpg"
           width={"500px"}
@@ -33,7 +34,9 @@ function App() {
         <button className="mx-3" onClick={subtract}>
           Subtract {count}
         </button>
-      </div>
+      </div> */}
+
+      <BgChanger />
     </>
   );
 }
