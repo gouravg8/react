51e7d121c80c920f265cfccd8d2868dcbe88eb58
Gourav soni <gouravsoni8476@gmail.com>
2024-03-14 22:15:15 +0530

    Created InputBox compo

---
+++ day1/01vite/src/components/CurrencyConvo/InputBox.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+function InputBox({
+  label,
+  amount,
+  onAmountChanged,
+  currencyOptions = [],
+  onCurrencyChanged,
+  selectCurrency = "usd",
+  className = "",
+  minValue,
+}) {
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+      <div className="w-1/2">
+        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <input
+          value={amount}
+          onChange={(e) => onAmountChanged && onAmountChanged(e.target.value)}
+          className="outline-none w-full bg-transparent py-1.5"
+          type="number"
+          placeholder="Amount"
+          min={minValue}
+        />
+      </div>
+      <div className="w-1/2 flex flex-wrap justify-end text-right">
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+          onChange={(e) =>
+            onCurrencyChanged && onCurrencyChanged(e.target.value)
+          }
+          value={selectCurrency}
+        >
+          {/* {console.log("this is currency options", currencyOptions)} */}
+          {currencyOptions.map((curr) => (
+            <option key={curr} value={curr}>
+              {curr}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default InputBox;
+InputBox.propTypes = {
+  label: PropTypes.string.isRequired,
+  amount: PropTypes.number.isRequired,
+  onAmountChanged: PropTypes.func.isRequired,
+  currencyOptions: PropTypes.arrayOf(PropTypes.string),
+  onCurrencyChanged: PropTypes.func,
+  selectCurrency: PropTypes.string,
+  className: PropTypes.string,
+  minValue: PropTypes.number,
+};
