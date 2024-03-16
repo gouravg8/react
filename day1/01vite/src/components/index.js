578f8c0123ed684faa333f38235867720b07d513
Gourav soni <gouravsoni8476@gmail.com>
2024-03-16 19:09:47 +0530

    exports for all components

---
+++ day1/01vite/src/components/index.js
@@ -0,0 +1,7 @@
+import CurrencyConvo from "./CurrencyConvo/CurrencyConvo";
+import PassGen from "./PassGen";
+import BgChanger from "./BgChanger";
+import Home from "./Home";
+import { homeLoader } from "./Home";
+
+export { CurrencyConvo, PassGen, BgChanger, Home, homeLoader };
